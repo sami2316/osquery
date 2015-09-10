8868b404b2fba49d49763192da67e5e18d5b1ea2
sami2316 <sami2316@users.noreply.github.com>
2015-09-10 15:34:21 +0500

    Create BrokerQueryPlugin.cpp

---
+++ osquery/main/BrokerQueryPlugin.cpp
@@ -0,0 +1,19 @@
+/* 
+ *  Copyright (c) 2015, nexGIN, RC.
+ *  All rights reserved.
+ * 
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "BrokerQueryPlugin.h"
+
+
+Status BrokerQueryManagerPlugin::genConfig(std::map<std::string,
+        std::string>& config)
+{
+    return Status(0,"Not Used");   
+}
+
